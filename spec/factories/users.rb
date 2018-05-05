8389e5f804af29f636c740f3a27f546f92f4888e
Leonid Medovyy <lmedovyy@gmail.com>
2018-05-05 09:50:49 -0700

    users test now passing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,10 +5,23 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     username { Faker::Internet.unique.email }
     password { Faker::Internet.password }
+    phone_number '5202222222'
+    country 'us'
+
+    after(:create) do |user|
+      user.phone_verified = true
+      user.save
+    end
 
     trait :admin do
       after(:create) do |user|
         user.add_role(:admin)
+      end
+    end
+
+    trait :banned do
+      after(:create) do |user|
+        user.add_role(:banned)
       end
     end
   end
