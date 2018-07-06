1ca449b71179800d5a05c6aa097618c33d7ddc81
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-06 09:07:04 -0700

    setting up redactor

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -20,9 +20,9 @@ Rails.application.config.content_security_policy do |policy|
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
   if Rails.env.development?
-    policy.script_src :self, :https, "http://cdn.segment.com", :unsafe_eval, :unsafe_inline
+    policy.script_src :self, :http, :https, "http://cdn.segment.com", :unsafe_eval, :unsafe_inline
   else
-    policy.script_src :self, :https, "http://cdn.segment.com", :unsafe_eval, :unsafe_inline
+    policy.script_src :self, :http, :https, "http://cdn.segment.com", :unsafe_eval, :unsafe_inline
     policy.font_src   :self, :https, :data
   end
 end
