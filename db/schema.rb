4c45de079b4f9bd441801e5153904d8f60bd4168
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-09 10:02:41 -0700

    added type to settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_154454) do
+ActiveRecord::Schema.define(version: 2018_07_09_170019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -185,6 +185,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_154454) do
     t.json "payload"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "taggings", force: :cascade do |t|
