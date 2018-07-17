f94bc7037c124c17786368956f1c488db585c2f4
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-17 11:05:57 -0700

    updated default scope for colors to exlude white

--- app/models/color.rb
+++ app/models/color.rb
@@ -3,4 +3,7 @@
 class Color < ApplicationRecord
   validates :name, presence: true
   has_many :categories
+
+  # we don't want white to be pulled in by default
+  default_scope -> { where.not(name: 'white')}
 end
