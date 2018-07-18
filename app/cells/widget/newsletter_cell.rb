099b4ab4c4f8ce532a6524e891f9b96df4dd997f
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-17 17:29:01 -0700

    refactored footer

--- app/cells/widget/newsletter_cell.rb
+++ app/cells/widget/newsletter_cell.rb
@@ -2,5 +2,13 @@
 
 module Widget
   class NewsletterCell < BaseCell
+
+    private
+
+    def email
+      return options[:current_user].email if options[:current_user]
+      ""
+    end
+
   end
 end
