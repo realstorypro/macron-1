32a3f185376068df0932780d627b33ca949128ae
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-19 22:17:11 -0700

    split up config files

--- config/initializers/01_config_bootloader.rb
+++ config/initializers/01_config_bootloader.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
-# Load Implementation Config
-Settings.add_source!(Rails.root.join("implementations").join("common/default.yml").to_s)
-Settings.add_source!(Rails.root.join("implementations").join("gravity.yml").to_s)
+# Load Core Config
+Settings.add_source!(Rails.root.join("core").join("defaults.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("auth.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("menu.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("colors.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("pages.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("tests.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("components.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views.yml").to_s)
+
 Settings.reload!
