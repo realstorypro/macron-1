d5f09166efdbf5cc91307c6a2bcefd688a4eb0ba
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-19 22:58:07 -0700

    further refactored settings

--- config/initializers/01_config_bootloader.rb
+++ config/initializers/01_config_bootloader.rb
@@ -8,6 +8,20 @@ Settings.add_source!(Rails.root.join("core").join("colors.yml").to_s)
 Settings.add_source!(Rails.root.join("core").join("pages.yml").to_s)
 Settings.add_source!(Rails.root.join("core").join("tests.yml").to_s)
 Settings.add_source!(Rails.root.join("core").join("components.yml").to_s)
-Settings.add_source!(Rails.root.join("core").join("views.yml").to_s)
+
+Settings.add_source!(Rails.root.join("core").join("views/defaults.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/profile.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/users.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/articles.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/discussions.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/advertisements.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/tags.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/categories.yml").to_s)
+
+Settings.add_source!(Rails.root.join("core").join("views/settings/general.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/settings/branding.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/settings/theme.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/settings/contact.yml").to_s)
+Settings.add_source!(Rails.root.join("core").join("views/settings/integration.yml").to_s)
 
 Settings.reload!
