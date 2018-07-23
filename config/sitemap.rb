f50808dc77546aae3ba27de8b5af0113371fbdb2
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-23 11:30:14 -0700

    hooked up sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,11 +5,11 @@ SitemapGenerator::Sitemap.default_host = "https://#{ENV['URL']}"
 SitemapGenerator::Sitemap.public_path = 'tmp/'
 
 # AWS Upload Config
-# SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
-#                                                                     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#                                                                     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#                                                                     fog_directory: ENV['AWS_S3_BUCKET'],
-#                                                                     fog_region: ENV['AWS_REGION'])
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+                                                                    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                                                    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+                                                                    fog_directory: ENV['AWS_S3_BUCKET'],
+                                                                    fog_region: ENV['AWS_REGION'])
 
 SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['AWS_S3_BUCKET']}.s3.amazonaws.com/"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
