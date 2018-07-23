309e915133cdee1e815ce913a85ed693836405b6
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-23 11:25:10 -0700

    hookup sitemap to work with videos

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "2.3.1"
 
 # Core
+gem "fog-aws"
 gem "rails", "~> 5.2.0"
 gem "bootsnap", ">= 1.1.0", require: false
 gem "puma", "~> 3.11"
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,15 +1,16 @@
 # rubocop:disable LineLength
+
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://#{ENV['URL']}"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
 
 # AWS Upload Config
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
-                                                                    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                                                                    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                                                                    fog_directory: ENV['AWS_S3_BUCKET'],
-                                                                    fog_region: ENV['AWS_REGION'])
+# SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+#                                                                     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#                                                                     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#                                                                     fog_directory: ENV['AWS_S3_BUCKET'],
+#                                                                     fog_region: ENV['AWS_REGION'])
 
-SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['AWS_S3_BUCKET']}.s3.amazonaws.com/"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
@@ -24,6 +25,11 @@ SitemapGenerator::Sitemap.create do
   add discussions_path, priority: 0.7, changefreq: 'daily'
   Discussion.find_each do |discussion|
     add discussion_details_path(discussion.category.slug, discussion.slug), lastmod: discussion.updated_at
+  end
+
+  add videos_path, priority: 0.7, changefreq: 'daily'
+  Video.find_each do |video|
+    add video_details_path(video.category.slug, video.slug), lastmod: video.updated_at
   end
 end
 
