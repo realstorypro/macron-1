9c8d6a5758fb55905ae8ac1b1980a9b652ba45de
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-23 20:01:29 -0700

    hooked up basic auth to sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -99,6 +99,11 @@ Rails.application.routes.draw do
         ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
   end if Rails.env.production?
 
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+
   mount Sidekiq::Web => "/sidekiq"
 
 end
