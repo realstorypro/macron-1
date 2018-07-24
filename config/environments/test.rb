734d66f8487559bb6268e4bd6f53d0d6cb3b02e4
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-24 09:58:39 -0700

    disabling mail delivery

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
+
+  # disabling mail delivery
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
