1ca4f3646f12ed40834fd45831225b73b1f31125
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-25 11:40:18 -0700

    made tests pass

---
+++ spec/factories/podcasts.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :podcast do
+    name { Faker::Name.name }
+    description { Faker::Name.name }
+    body { Faker::Demographic.race }
+    long_title { Faker::Name.name }
+    published_date { Faker::Date.forward(7) }
+    landscape_image { Faker::Avatar.image }
+    card_image { Faker::Avatar.image }
+    image_alt { Faker::Name.name }
+    audio "https://soundcloud.com/waximi_ackawala/reloj-de-campana-1"
+    association :category, factory: :category
+    after(:create) do |article|
+      create(:comment, commentable: article)
+    end
+  end
+end
