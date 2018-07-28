0f2f221318bfc60108c350ec10a758c4df2f518a
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-28 12:28:59 -0700

    setting up a generator

--- lib/generators/rails/theme_settings/theme_settings_generator.rb
+++ lib/generators/rails/theme_settings/theme_settings_generator.rb
@@ -1,3 +1,8 @@
 class Rails::ThemeSettingsGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
+
+  def copy_theme_settings_file
+    copy_file "view.yml", "core/views/settings/theme/#{file_name}.yml"
+
+  end
 end
