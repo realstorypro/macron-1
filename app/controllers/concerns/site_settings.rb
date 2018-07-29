6012d5d4ab565a28051c1bb7968601d25b1c4dab
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-29 13:26:06 -0700

    updated site settings to pull in all of the settings

--- app/controllers/concerns/site_settings.rb
+++ app/controllers/concerns/site_settings.rb
@@ -15,25 +15,44 @@ module SiteSettings
     def load_site_settings
       site_settings = $redis.get("site_settings")
 
-      if true || site_settings.nil?
+      if site_settings.nil?
         site_settings = Hash.new
 
         general_settings = SiteSettings::General.instance.payload
-        #branding_settings = SiteSettings::Branding.instance.payload
         contact_settings = SiteSettings::Contact.instance.payload
-        #theme_settings = SiteSettings::Theme.instance.payload
         integration_settings = SiteSettings::Integration.instance.payload
+        article_theme_settings = SiteSettings::Theme::Article.instance.payload
+        auth_theme_settings = SiteSettings::Theme::Authentication.instance.payload
+        branding_theme_settings = SiteSettings::Theme::Branding.instance.payload
+        discussion_theme_settings = SiteSettings::Theme::Discussion.instance.payload
+        footer_theme_settings = SiteSettings::Theme::Footer.instance.payload
+        global_theme_settings = SiteSettings::Theme::Global.instance.payload
+        header_theme_settings = SiteSettings::Theme::Header.instance.payload
+        homepage_theme_settings = SiteSettings::Theme::Homepage.instance.payload
+        podcast_theme_settings = SiteSettings::Theme::Podcast.instance.payload
+        video_theme_settings = SiteSettings::Theme::Video.instance.payload
 
         site_settings[:general] = general_settings
-        #site_settings[:branding] = branding_settings
         site_settings[:contact] = contact_settings
-        #site_settings[:theme] = theme_settings
         site_settings[:integration] = integration_settings
+
+        site_settings[:theme] = {}
+        site_settings[:theme][:article] = article_theme_settings
+        site_settings[:theme][:auth] = auth_theme_settings
+        site_settings[:theme][:branding] = branding_theme_settings
+        site_settings[:theme][:discussion] = discussion_theme_settings
+        site_settings[:theme][:footer] = footer_theme_settings
+        site_settings[:theme][:global] = global_theme_settings
+        site_settings[:theme][:header] = header_theme_settings
+        site_settings[:theme][:homepage] = homepage_theme_settings
+        site_settings[:theme][:podcast] = podcast_theme_settings
+        site_settings[:theme][:video] = video_theme_settings
 
         site_settings = site_settings.to_json
 
         $redis.set("site_settings", site_settings)
       end
+      
       @site_settings = JSON.parse(site_settings)
 
       # This isn't ideal as we're re-initing analytics with every call.
