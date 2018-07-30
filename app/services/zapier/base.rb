cc401692a0d25a75a71fe77e30d33dacb2ff4c39
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-30 09:57:06 -0700

    subscription refactored

--- app/services/zapier/base.rb
+++ app/services/zapier/base.rb
@@ -4,6 +4,7 @@
 module Zapier
   class Base
     include HTTParty
+    include SiteSettingsHelper
 
     attr_accessor :error_message, :response, :code, :body
     attr_reader :resource
@@ -12,6 +13,7 @@ module Zapier
       @resource = resource
       load_site_settings
     end
+
 
     def post_to_zapier
       self.response = call_operation
@@ -33,38 +35,11 @@ module Zapier
 
     private
 
-      # TODO: we don't want to load this guy twice
-
       def load_site_settings
         site_settings = $redis.get("site_settings")
-
-        if site_settings.nil?
-          site_settings = Hash.new
-
-          general_settings = SiteSettings::General.instance.payload
-          branding_settings = SiteSettings::Branding.instance.payload
-          contact_settings = SiteSettings::Contact.instance.payload
-          theme_settings = SiteSettings::Theme.instance.payload
-          integration_settings = SiteSettings::Integration.instance.payload
-
-          site_settings.merge!(general_settings)
-          site_settings.merge!(branding_settings)
-          site_settings.merge!(contact_settings)
-          site_settings.merge!(theme_settings)
-          site_settings.merge!(integration_settings)
-
-
-          site_settings = { payload: site_settings }.to_json
-
-          $redis.set("site_settings", site_settings)
-        end
-
         @site_settings = JSON.parse(site_settings)
       end
 
-      def ss(name)
-        @site_settings["payload"][name.to_s]
-      end
   end
 end
 # rubocop:enable GlobalVars
