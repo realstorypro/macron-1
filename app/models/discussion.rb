cce13f4c386a304aa7887de9da55b97c3b9bc1e6
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-31 15:36:04 -0700

    refactored discussion

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
 class Discussion < Entry
-  content_attr :long_title, :string
-  content_attr :long_summary, :text
-  content_attr :landscape_image, :string
-  content_attr :image_alt, :string
-  content_attr :body, :text
-  content_attr :link, :text
-
-  validates_presence_of :long_title, :long_summary,
-                        :body, :category, :description
+  include Autoloadable
+  validates_presence_of :category
   paginates_per 25
 end
