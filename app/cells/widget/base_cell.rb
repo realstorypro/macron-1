789e7b56291538d9fb7f755c29c9eca4f0a35982
Leonid Medovyy <lmedovyy@gmail.com>
2018-07-31 22:45:30 -0700

    refactoring out base cell

--- app/cells/widget/base_cell.rb
+++ app/cells/widget/base_cell.rb
@@ -23,23 +23,15 @@ module Widget
       s("components.#{options[:component]}.path")
     end
 
-    # TODO this is code duplication. find  way to remove this and only rely on site_settings_helper
     def site_setting(path, opts = {})
-      # split the path into an array
-      path_array = path.split(".")
-      value = nil
-      path_array.each_with_index do |fragment, index|
-        value = options[:site_settings][fragment] if index.eql? 0
-        value = value[fragment] unless index.eql? 0
-        raise "fragment `#{fragment}` does not exist within `#{path}`" if value.nil? && opts[:fatal_exception]
-        return nil if value.nil?
-      end
-      value
+      settings ||= SettingInterface.new(options[:site_settings])
+      settings.fetch_setting(path, fatal_exception: fatal)
     end
 
     # shortcut for site settings
     def ss(path)
-      site_setting(path, fatal_exception: true)
+      settings ||= SettingInterface.new(options[:site_settings])
+      settings.fetch_setting(path, fatal_exception: true)
     end
   end
 end
