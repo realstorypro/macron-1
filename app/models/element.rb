8d33c5285179a5e6f2670a40c3a1a1e8fa172e63
Leonid Medovyy <lmedovyy@gmail.com>
2018-08-25 11:08:00 -0700

    super polymorphic association enabled!

--- app/models/element.rb
+++ app/models/element.rb
@@ -3,13 +3,10 @@
 class Element < ApplicationRecord
   include Payloadable
 
-  has_one :page_element, dependent: :destroy
-  has_one :page, through: :page_elements
-
-  belongs_to :elementable, polymorphic: true
+  belongs_to :elementable, polymorphic: true, optional: true
   has_many :elements, -> { order(:position) }, as: :elementable
 
-  belongs_to :page
+  belongs_to :page, optional: true
 
   def self.policy_class
     MetaPolicy
