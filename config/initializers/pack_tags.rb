f79377dcbad5cceb2cb266fe357ad70a0a62939f
Leonid Medovyy <leonid@logik.works>
2018-12-25 20:11:05 -0700

    removing the asset host from pack urls

--- config/initializers/pack_tags.rb
+++ config/initializers/pack_tags.rb
@@ -3,11 +3,13 @@
 module Webpacker
   module Helper
     def stylesheet_pack_url(name)
-      Rails.application.config.action_controller.asset_host.to_s + source_from_pack(name, type: :stylesheet)
+      # Rails.application.config.action_controller.asset_host.to_s + source_from_pack(name, type: :stylesheet)
+      '/' +  source_from_pack(name, type: :stylesheet)
     end
 
     def javascript_pack_url(name)
-      Rails.application.config.action_controller.asset_host.to_s + source_from_pack(name, type: :javascript)
+      # Rails.application.config.action_controller.asset_host.to_s + source_from_pack(name, type: :javascript)
+      '/' + source_from_pack(name, type: :javascript)
     end
 
     private
