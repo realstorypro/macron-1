52bea71f8b83ee403b3a7a8cadc2f5e2926cf941
Leonid Medovyy <leonid@logik.works>
2018-12-31 09:45:44 -0700

    memoizing analytics

--- app/controllers/concerns/site_settings.rb
+++ app/controllers/concerns/site_settings.rb
@@ -13,11 +13,13 @@ module SiteSettings
 
     def load_site_settings
       @site_settings = $site_setting_interface.fetch_json
-      $site_setting_interface.update unless @site_settings.nil?
 
-      # This isn't ideal as we're re-initing analytics with every call.
-      # It would be nice to find a way to memoize it.
-      @analytics = Segment::Analytics.new(write_key: ss("integration.segment_server_key"))
+      if @site_settings.nil?
+        $site_setting_interface.update
+        @analytic = Segment::Analytics.new(write_key: ss("integration.segment_server_key"))
+      end
+
+      @analytic ||= Segment::Analytics.new(write_key: ss("integration.segment_server_key"))
     end
 end
 
