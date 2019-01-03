ec2fa3dd5b42e35f19fdb9baa0274967bb69b088
Leonid Medovyy <leonid@logik.works>
2019-01-03 15:37:08 -0700

    refactoring site_settings

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,8 +2,6 @@
 
 # rubocop:disable GlobalVars
 
-#$redis = Redis::Namespace.new("aquarius", redis: Redis.new)
-#$site_setting_interface = SiteSettingInterface.new($redis, "site_settings")
 $site_setting_interface = SiteSettingInterface.instance
 
 # rubocop:enable GlobalVars
