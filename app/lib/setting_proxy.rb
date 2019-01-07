4db1acd1cfef9cdb83d2d076a5b0a45ac5da5f18
Leonid Medovyy <leonid@logik.works>
2019-01-07 08:02:44 -0700

    refactoring settings proxy

--- app/lib/setting_proxy.rb
+++ app/lib/setting_proxy.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
+# s (args) -> settings('')
+# ss -> settings('site.(args)')
 
 # Class acting as a proxy between Setting and SiteSettings
 class SettingProxy
   include Singleton
 
   def initialize
+  end
+
+  def fetch(path, params)
+    if path.split(".")[0].include?("site")
+      fetch_site_settings path, params
+    else
+      fetch_settings path, params
+    end
   end
 
   def fetch_settings(path, options = {})
