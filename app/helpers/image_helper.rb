27cccd1236518d0f36148b42c6a5a8665ed0bc0c
Leonid Medovyy <leonid@logik.works>
2019-01-16 09:18:03 -0700

    adding alt tags

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -2,8 +2,8 @@
 
 module ImageHelper
   # Preloads Image for the Image Preload Widget
-  def preload_image(src, klass = nil)
-    rtr_str = image_tag ""
+  def preload_image(src, klass = nil, alt_text = nil)
+    rtr_str = image_tag "", alt: alt_text
     rtr_str << content_tag(:div, class: "ui dimmer active") do
       content_tag :div, class: "ui small loader" do
       end
