0cab5907a005d821f01bcc495cb999be70be3570
Leonid Medovyy <leonid@logik.works>
2019-02-12 05:21:09 -0700

    updating profile style

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,9 +3,13 @@
 module UserHelper
   # renders profile avatar if one has been uploaded
   # todo: remove helper and utilize the avatar function on user model
-  def avatar_thumbnail(user = current_user)
+  def avatar_thumbnail(user = current_user, resize = true)
     if user.profile.respond_to?(:avatar) && user.profile.avatar.nil? == false
-      user.profile.avatar.gsub("-/resize/600x600/", "-/resize/70x70/-/quality/lightest/")
+      if resize
+        user.profile.avatar.gsub("-/resize/600x600/", "-/resize/70x70/-/quality/lightest/")
+      else
+        user.profile.avatar
+      end
     else
       "default-avatar.jpg"
     end
