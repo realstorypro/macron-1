ccb54f98069d66efce767b4a8c6862082f2fa245
Leonid Medovyy <leonid@logik.works>
2019-04-06 15:55:05 +0530

    cleaning up

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,12 +1,17 @@
 class API::V1::ActivitiesController < ApplicationController
 
-  # returns activities for the current user
+  # returns all activities
   def index
+    where_clause = {}
+    where_clause['id'] = params[:activities] if params[:activities]
     @activities = PublicActivity::Activity.order("created_at desc")
+                      .where(where_clause)
                       .preload(:owner, :trackable)
+                      .offset(params[:offset])
+                      .limit(10)
   end
 
-  # returns single activity based on activity id
+  # returns activities for a single user
   def show
     where_clause = {}
     where_clause['owner_id'] = params[:id]
