2a17e5280a97dd3d31674ec609bcac8c9111ae34
Leonid Medovyy <leonid@logik.works>
2019-04-08 21:37:28 +0530

    setting up vue cells

---
+++ app/cells/vue/follower_cell.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Vue
+  class FollowerCell < Cell::ViewModel
+    include DcUi::Helpers
+  end
+end
---
+++ app/cells/vue/like_cell.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Vue
+  class LikeCell < Cell::ViewModel
+    include DcUi::Helpers
+  end
+end
