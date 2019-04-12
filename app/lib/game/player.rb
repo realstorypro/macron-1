bc8ae4d063c893c6f06a962ed5d0fd639cbb0767
Leonid Medovyy <leonid@logik.works>
2019-04-12 22:26:05 +0530

    building a gaming library

--- app/lib/game/player.rb
+++ app/lib/game/player.rb
@@ -3,104 +3,108 @@
 module Game
   class Player
 
-    def initialize(options = {})
-
+    def initialize(player)
+      @player = player
     end
 
-    # returns paths and points for a given user
-    # path may be passed as a filter
-    def points(player, path = nil)
-      return false unless path_exists?(path)
+    # returns the current player state
+    def state
+      state = []
 
-      return player.score_points(category: path).sum(:num_points) if path
-      player.score_points.sum(:num_points)
-    end
-
-    # adds points to the user
-    # returns the new point value for the passed path
-    def add_points(player, path, amount)
-      return false unless path_exists?(path)
-
-      player.add_points(amount, category: path)
-      points(player, path)
-    end
-
-    # subtracts points from the user
-    def subtract_points(player, path, amount)
-      return false unless path_exists?(path)
-
-      player.subtract_points(amount, category: path)
-      points(player, path)
-    end
-
-    # returns all of the supporters the user has
-    def supporters(player)
-      player.user_followers
-    end
-
-    def supporters_count(player)
-      player.followers_by_type_count('User')
-    end
-
-    # adds a supporter to the user
-    def add_supporter(player, supporter)
-      supporter.follow(player)
-    end
-
-    # remove a supporter to the user
-    def remove_supporter(player, supporter)
-      supporter.stop_following(player)
-    end
-
-
-    # returns all available spells
-    def available_spells?(player)
-      path_state = []
-
+      # iterate through all of the paths
       all_paths.each do |path|
-        points = OpenStruct.new
-        points.name = path[0].to_s
+        current_path = OpenStruct.new
+        current_path.name = path[0].to_s
 
         # get points
-        points.points = points(player, points.name)
+        current_path.points = points(current_path.name)
 
         # get level
-        points.level = get_level(points.points)
+        current_path.level = get_level(current_path.points)
 
-        # get spells appropriate for the level
-
-        path_state << points
+        state << current_path
       end
 
-      path_state
-
-      spells = []
-      path_state.each do |path|
+      # collect spells appropriate for the level
+      state.each do |path|
         path.spells = []
         s("spells.#{path.name}").each do |spell|
           path.spells << spell if spell[1].level <= path.level
         end
       end
 
-      path_state
+      state
+    end
+
+    # returns paths and points for a given user
+    # path may be passed as a filter
+    def points(path = nil)
+      return false unless path_exists?(path)
+
+      return @player.score_points(category: path).sum(:num_points) if path
+      @player.score_points.sum(:num_points)
+    end
+
+    # adds points to the user
+    # returns the new point value for the passed path
+    def add_points(path, amount)
+      return false unless path_exists?(path)
+
+      @player.add_points(amount, category: path)
+    end
+
+    # subtracts points from the user
+    def subtract_points(path, amount)
+      return false unless path_exists?(path)
+
+      @player.subtract_points(amount, category: path)
+    end
+
+    # returns all of the supporters the user has
+    def supporters
+      @player.user_followers
+    end
+
+    def supporters_count
+      @player.followers_by_type_count('User')
+    end
+
+    # adds a supporter to the user
+    def add_supporter(supporter)
+      supporter.follow(@player)
+    end
+
+    # remove a supporter to the user
+    def remove_supporter(supporter)
+      supporter.stop_following(@player)
+    end
+
+
+    # returns all available spells
+    # @param [Object] player's path
+    def available_spells?(path = nil)
+      spells = []
+
+      current_state = state
+      current_state.each do |path|
+        path.spells.each { |spell| spells << spell }
+      end
+
+      spells
     end
 
     # a spell authorization method
-    def can_cast?(user)
+    def can_cast?
 
     end
 
     # casts a spell on a subject
-    def cast_spell!(user, subject, spell)
+    def cast_spell!(subject, spell)
 
     end
 
     # removes a spell from a subject
-    def remove_spell!(user, subject, spell)
-
-    end
-
-    def level_up(user, trait)
+    def remove_spell!(subject, spell)
 
     end
 
