31b5058dcf340381638d856e212312a88c9b707b
Leonid Medovyy <leonid@logik.works>
2019-06-25 23:12:27 +0530

    trying to remove activites

--- db/migrate/20190411072624_create_activities.rb
+++ db/migrate/20190411072624_create_activities.rb
@@ -1,6 +1,6 @@
 class CreateActivities < ActiveRecord::Migration[5.2]
   def change
-    drop_table :activities
+    #drop_table :activities
     create_table :activities do |t|
       t.references :actable, polymorphic: true, index:true
       t.references :subjectable, polymorphic: true, index:true
