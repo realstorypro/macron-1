9d8d277896948a8f8d4012f69e30dfa2e7f25cac
Leonid Medovyy <leonid@logik.works>
2019-09-09 20:18:25 -0700

    built an embed plugin

--- app/frontend/shared/js/plugins/redactor/embeddable.js
+++ app/frontend/shared/js/plugins/redactor/embeddable.js
@@ -1,37 +1,44 @@
 (function($R)
 {
     $R.add('plugin', 'embeddable', {
-        modals: {
-            'embeddable': '<form action="">'
-                + '<div class="form-item">'
-                + '<label>## embeddable-label ##</label>'
-                + '<textarea name="text" style="height: 200px;"></textarea>'
-                + '</div>'
-                + '</form>'
-        },
         translations: {
             en: {
-                "embeddable": "embed",
-                "embeddable-label": "Please enter the URL to embed"
+                "embeddable": "Embed",
+                "embeddable-html-code": "Embed Link"
             }
+        },
+        modals: {
+            'embeddable':
+                '<form action=""> \
+                    <div class="form-item"> \
+                        <label for="modal-embeddable-input">## embeddable-html-code ##</label> \
+                        <textarea id="modal-embeddable-input" name="embeddable" style="height: 200px;"></textarea> \
+                    </div> \
+                </form>'
         },
         init: function(app)
         {
-            // define app
             this.app = app;
-
-            // define services
             this.lang = app.lang;
+            this.opts = app.opts;
             this.toolbar = app.toolbar;
+            this.component = app.component;
             this.insertion = app.insertion;
+            this.inspector = app.inspector;
+            this.selection = app.selection;
         },
-
         // messages
         onmodal: {
             embeddable: {
                 opened: function($modal, $form)
                 {
-                    $form.getField('text').focus();
+                    $form.getField('embeddable').focus();
+
+                    if (this.$currentItem)
+                    {
+                        var code = decodeURI(this.$currentItem.attr('data-embeddable-code'));
+                        $form.getField('embeddable').val(code);
+                    }
                 },
                 insert: function($modal, $form)
                 {
@@ -40,43 +47,169 @@
                 }
             }
         },
+        oncontextbar: function(e, contextbar)
+        {
+            var data = this.inspector.parse(e.target)
+            if (data.isComponentType('embeddable'))
+            {
+                var node = data.getComponent();
+                var buttons = {
+                    "edit": {
+                        title: this.lang.get('edit'),
+                        api: 'plugin.embeddable.open',
+                        args: node
+                    },
+                    "remove": {
+                        title: this.lang.get('delete'),
+                        api: 'plugin.embeddable.remove',
+                        args: node
+                    }
+                };
+
+                contextbar.set(e, node, buttons, 'bottom');
+            }
+        },
+        onbutton: {
+            embeddable: {
+                observe: function(button)
+                {
+                    this._observeButton(button);
+                }
+            }
+        },
 
         // public
         start: function()
         {
-            // create the button data
-            var buttonData = {
+            var obj = {
                 title: this.lang.get('embeddable'),
-                api: 'plugin.embeddable.open'
+                api: 'plugin.embeddable.open',
+                observe: 'embeddable'
             };
 
-            // create the button
-            var $button = this.toolbar.addButton('embeddable', buttonData);
+            var $button = this.toolbar.addButton('embeddable', obj);
         },
         open: function()
         {
+            this.$currentItem = this._getCurrent();
+
             var options = {
                 title: this.lang.get('embeddable'),
                 width: '600px',
                 name: 'embeddable',
                 handle: 'insert',
                 commands: {
-                    insert: { title: this.lang.get('insert') },
+                    insert: { title: (this.$currentItem) ? this.lang.get('save') : this.lang.get('insert') },
                     cancel: { title: this.lang.get('cancel') }
                 }
             };
 
             this.app.api('module.modal.build', options);
         },
+        remove: function(node)
+        {
+            this.component.remove(node);
+        },
 
         // private
+        _getCurrent: function()
+        {
+            var current = this.selection.getCurrent();
+            var data = this.inspector.parse(current);
+            if (data.isComponentType('embeddable'))
+            {
+                return this.component.build(data.getComponent());
+            }
+        },
         _insert: function(data)
         {
             this.app.api('module.modal.close');
 
-            if (data.text.trim() === '') return;
+            if (data.embeddable.trim() === '' || (this._isHtmlString(data.embeddable)))
+            {
+                return;
+            }
 
-            this.insertion.insertText('[embed]'+data.text+'[/embed]');
+            var embed_text = '[embed]'+ data.embeddable +'[/embed]';
+            var html = document.createTextNode(embed_text);
+            var $component = this.component.create('embeddable', html);
+            $component.attr('data-embeddable-code', encodeURI(data.embeddable.trim()));
+            this.insertion.insertHtml($component);
+
+        },
+        _isHtmlString: function(html)
+        {
+            return !(typeof html === 'string' && !/^\s*<(\w+|!)[^>]*>/.test(html));
+        },
+        _observeButton: function(button)
+        {
+            var current = this.selection.getCurrent();
+            var data = this.inspector.parse(current);
+
+            if (data.isComponentType('table')) button.disable();
+            else button.enable();
+        }
+    });
+})(Redactor);
+(function($R)
+{
+    $R.add('class', 'embeddable.component', {
+        mixins: ['dom', 'component'],
+        init: function(app, el)
+        {
+            this.app = app;
+
+            // init
+            return (el && el.cmnt !== undefined) ? el : this._init(el);
+        },
+        getData: function()
+        {
+            return {
+                html: this._getHtml()
+            };
+        },
+
+        // private
+        _init: function(el)
+        {
+            if (typeof el !== 'undefined')
+            {
+                var $node = $R.dom(el);
+                var $embeddable = $node.closest('div.embed');
+                if ($embeddable.length !== 0)
+                {
+                    this.parse($embeddable);
+                }
+                else
+                {
+                    this.parse('<div class="embed">');
+                    this.html(el);
+                }
+            }
+            else
+            {
+                this.parse('<div class="embed">');
+            }
+
+
+            this._initWrapper();
+        },
+        _getHtml: function()
+        {
+            var $wrapper = $R.dom('<div>');
+            $wrapper.html(this.html());
+            $wrapper.find('.redactor-component-caret').remove();
+
+            return $wrapper.html();
+        },
+        _initWrapper: function()
+        {
+            this.addClass('redactor-component');
+            this.attr({
+                'data-redactor-type': 'embeddable',
+                'tabindex': '-1',
+                'contenteditable': false
+            });
         }
     });
 })(Redactor);
