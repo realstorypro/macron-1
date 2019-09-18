83f758517d550320edfdd086cd366115815d8df9
Leonid Medovyy <leonid@logik.works>
2019-09-18 15:07:27 -0700

    further setting up the elements generator

--- lib/generators/rails/elements/elements_generator.rb
+++ lib/generators/rails/elements/elements_generator.rb
@@ -5,7 +5,11 @@ class Rails::ElementsGenerator < Rails::Generators::NamedBase
   require "yaml"
   source_root File.expand_path("templates", __dir__)
 
-  def copy_settings_file
+  class_option :desc, type: :string, default: 'Element Description'
+
+  def copy_element_file
+    @description= options['desc']
+
     # copy model files
     copy_file "model.rb", "app/models/elements/#{file_name}.rb"
     gsub_file "app/models/elements/#{file_name}.rb", "<~~ class_name ~~>", class_name
@@ -14,6 +18,7 @@ class Rails::ElementsGenerator < Rails::Generators::NamedBase
     copy_file "element_component.yml", "core/elements/#{file_name}.yml"
     gsub_file "core/elements/#{file_name}.yml", "<~~ lowercase_name ~~>", class_name.downcase
     gsub_file "core/elements/#{file_name}.yml", "<~~ class_name ~~>", class_name
+    gsub_file "core/elements/#{file_name}.yml", "<~~ element_description ~~>", @description
 
   end
 end
