732bc1a50ca1f43fb492a95f5e89b3aed70358b0
Leonid Medovyy <leonid@logik.works>
2019-09-18 15:01:38 -0700

    settiong up the elements generator

--- lib/generators/rails/elements/elements_generator.rb
+++ lib/generators/rails/elements/elements_generator.rb
@@ -6,10 +6,15 @@ class Rails::ElementsGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
 
   def copy_settings_file
-    # copy view files
     # copy model files
     copy_file "model.rb", "app/models/elements/#{file_name}.rb"
     gsub_file "app/models/elements/#{file_name}.rb", "<~~ class_name ~~>", class_name
+
+    # copy the component file
+    copy_file "element_component.yml", "core/elements/#{file_name}.yml"
+    gsub_file "core/elements/#{file_name}.yml", "<~~ lowercase_name ~~>", class_name.downcase
+    gsub_file "core/elements/#{file_name}.yml", "<~~ class_name ~~>", class_name
+
   end
 end
 
