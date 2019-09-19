a6255beda965ee8431e5b60feaed2a21297be835
Leonid Medovyy <leonid@logik.works>
2019-09-18 22:48:32 -0700

    adjusting richtext

--- app/cells/elements/richtext_cell.rb
+++ app/cells/elements/richtext_cell.rb
@@ -2,5 +2,10 @@
 
 module Elements
   class RichtextCell < BaseElementCell
+    def formatted_text
+      model.rich.gsub!("<ul>","<ul class='ui list'>")
+      model.rich.gsub!("<ol>","<ol class='ui list'>")
+      model.rich
+    end
   end
 end
