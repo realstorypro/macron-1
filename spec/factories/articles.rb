6a730867e2e5ce7f73b2d3d5bc81320926d6711e
Leonid Medovyy <leonid@logik.works>
2019-09-20 14:09:42 -0700

    updated default theme settings & test factories

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -4,11 +4,7 @@ FactoryBot.define do
   factory :article do
     name { Faker::Name.name }
     description { Faker::Name.name }
-    body { Faker::Demographic.race }
-    long_title { Faker::Name.name }
-    long_summary { Faker::Demographic.race }
     published_date { Faker::Date.forward(7) }
-    fullscreen_image { Faker::Avatar.image }
     landscape_image { Faker::Avatar.image }
     card_image { Faker::Avatar.image }
     image_alt { Faker::Name.name }
--- spec/factories/discussions.rb
+++ spec/factories/discussions.rb
@@ -4,9 +4,6 @@ FactoryBot.define do
   factory :discussion do
     name { Faker::Name.unique.name }
     description { Faker::Name.name }
-    body { Faker::Demographic.race }
-    long_title { Faker::Name.name }
-    long_summary { Faker::Demographic.race }
     published_date { Faker::Date.forward(7) }
     association :category, factory: :category
     association :user
