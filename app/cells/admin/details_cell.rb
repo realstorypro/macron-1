f4ea46d46c481a6534ab6be383bfda0a7ba09599
Leonid Medovyy <leonid@logik.works>
2019-09-20 13:17:39 -0700

    updated the details table

--- app/cells/admin/details_cell.rb
+++ app/cells/admin/details_cell.rb
@@ -7,12 +7,35 @@ module Admin
     end
 
     def rows
+      @header_locations = []
+      @number_of_fields = 0
+      options[:fields].each_with_index do |field , index|
+        @header_locations << index if field_type(field) == 'header'
+        @number_of_fields+=1
+      end
+
       render
     end
 
     def row(field)
       render(locals: { field: field })
     end
+
+    def render_table(index)
+      number_of_headers = @header_locations.length
+      header_position = @header_locations.find_index(index-1)
+
+      @from = @header_locations[header_position]
+
+      if (number_of_headers - 1) == header_position
+        @to = @number_of_fields
+      else
+        @to = @header_locations[header_position+1]
+      end
+
+      render
+    end
+
 
     def current_user
       options[:user]
