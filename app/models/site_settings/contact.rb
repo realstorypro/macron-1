298a573bc3980fdcfb2bb105e344b826cd3467e6
Leonid Medovyy <leonid@logik.works>
2019-09-23 17:53:34 -0700

    updating contact and tos

--- app/models/site_settings/contact.rb
+++ app/models/site_settings/contact.rb
@@ -5,7 +5,6 @@ module SiteSettings
     include Autoloadable
 
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, allow_blank: true
-    validates :facebook, url: { schemes: ["https"] }, allow_blank: true
     validates :website, url: { schemes: %w(http https) }, allow_blank: true
   end
 end
