27508c15f4897f2c70803324b14ffea3c0a491e0
Leonid Medovyy <leonid@logik.works>
2019-09-26 19:46:29 -0700

    added promo tracking

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -5,9 +5,10 @@ class DisplayController < MetaController
   before_action :fetch_categories, only: [:index]
   before_action :set_index_seo_meta, only: [:index]
   before_action :find_related_content, only: [:show]
-  before_action :find_related_ad, only: [:show]
+  before_action :find_related_promo, only: [:show]
   before_action :set_show_seo_meta, :set_twitter_meta, :set_og_meta, :set_article_meta, only: [:show]
-  after_action :record_view, only: [:show]
+  after_action :track_view, only: [:show]
+  after_action :track_promo, only: [:show]
   after_action :store_location, only: %i[index show]
   helper_method :index_path, :show_path
   layout "layouts/client"
@@ -30,7 +31,7 @@ class DisplayController < MetaController
   end
 
   private
-    def find_related_ad
+    def find_related_promo
       @promo = Tag.find_by_id(@entry.tags.map(&:id))
                  &.promotions&.order(Arel.sql("random()"))&.limit(1)
       @promo = Promotion.order(Arel.sql("random()")).limit(1) if @promo.blank?
@@ -50,7 +51,7 @@ class DisplayController < MetaController
       end
     end
 
-    def record_view
+    def track_view
       track(
         event: "Viewed Content",
         props: {
@@ -63,10 +64,21 @@ class DisplayController < MetaController
         }
       )
     end
+  
+    def track_promo
+      return true unless @promo
+      track(
+          event: "Promo Viewed",
+          props: {
+            id: @promo.last.id,
+            name: @promo.last.name,
+          }
+      )
+    end
 
     def store_location
-      session[:current_location] = request.fullpath
-    end
+        session[:current_location] = request.fullpath
+      end
 
     def fetch_categories
       @categories = Category.joins(params[:component].downcase.pluralize.to_sym).distinct(:id).order(:name)
