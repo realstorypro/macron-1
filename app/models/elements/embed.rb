4e29075b3f03dc9b60bf948552c30a0b49ceb14a
Leonid Medovyy <leonid@logik.works>
2019-10-01 14:46:10 -0700

    updating the image and embed elements; updating the showe views for the article, and layouts

--- app/models/elements/embed.rb
+++ app/models/elements/embed.rb
@@ -4,8 +4,10 @@ module Elements
   class Embed < Element
     include Autoloadable
     content_attr :embedded, :string
+    content_attr :embedded_amp, :string
 
     before_save :embed
+    before_save :embed_amp
 
     def embed
       unless url.blank?
@@ -19,5 +21,18 @@ module Elements
         self.embedded = parsed_response
       end
     end
+
+    def embed_amp
+      unless url.blank?
+        parsed_response = HTTParty
+                              .get("https://iframe.ly/api/iframely?url=#{self.url}&amp=1&api_key=#{ENV['IFRAMELY_API']}")
+                              .parsed_response["html"]
+        unless parsed_response
+          parsed_response = false
+        end
+
+        self.embedded_amp = parsed_response
+        end
+      end
   end
 end
