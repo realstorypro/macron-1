17f00376b9cf44a273c1214d02cbc56fe6c23b65
Leonid Medovyy <leonid@logik.works>
2019-10-04 22:38:14 -0700

    setting up the video component

--- app/models/video.rb
+++ app/models/video.rb
@@ -8,33 +8,6 @@ class Video < Entry
   before_save :embed_video
 
   validates_presence_of :category
-  validates :video, url: true
-  validate :approved_video_provider
 
   paginates_per 5
-
-  def approved_video_provider
-    services = %w[vimeo youtube]
-    return true if services.any? { |service| video.include? service }
-    errors.add(:video, "Only Vimeo and Youtube URLs are allowed")
-  end
-
-  def embed_video
-    # vimeo post processing
-    if video.include?("vimeo")
-      self.embedded_video = video.gsub("vimeo.com/", "player.vimeo.com/video/")
-    end
-
-    # youtube post processing
-    if video.include?("youtube")
-      # makes regular video embeddable
-      self.embedded_video = video.gsub("www.youtube.com/watch?v=", "www.youtube.com/embed/")
-
-      # removes the ampersand from a string
-      ampersand_location = self.embedded_video.index("&")
-      if ampersand_location
-        self.embedded_video = self.embedded_video[0..(ampersand_location - 1)]
-      end
-    end
-  end
 end
