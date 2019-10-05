12b432f08976531f849f51a568d2650695c9c5ec
Leonid Medovyy <leonid@logik.works>
2019-10-04 22:47:26 -0700

    setting up the basics for the video header

---
+++ app/cells/elements/videoheader_cell.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Elements
+  class VideoheaderCell < HeaderCell
+  end
+end
---
+++ app/models/elements/videoheader.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Elements
+  class Videoheader < Element
+    include Autoloadable
+  end
+end
