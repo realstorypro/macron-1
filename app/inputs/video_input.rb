f069a500e79020c4f30b47df6d5fa2bf8c904c1b
Leonid Medovyy <leonid@logik.works>
2019-10-05 11:28:37 -0700

    setting up basic videos

--- app/inputs/video_input.rb
+++ app/inputs/video_input.rb
@@ -1,20 +1,9 @@
 # frozen_string_literal: true
 
-# custom 'rich' input type for simple_form
-class ImageInput < SimpleForm::Inputs::Base
+# custom 'video' input type for simple_form
+class VideoInput < SimpleForm::Inputs::Base
   include SettingsHelper
   def input(_wrapper_options)
-    image_size = size(attribute_name)
-    @builder.uploadcare_field(attribute_name, data: { }).to_s.html_safe
+    @builder.uploadcare_field(attribute_name, data: {tabs: 'file camera url gphotos instagram'}).to_s.html_safe
   end
-
-  private
-    def size(attribute_name)
-      component = options[:component]
-      size = settings("views.#{component}.new.#{attribute_name}.size")
-
-      return "free" unless size
-
-      size[0] + "x" + size[1]
-    end
 end
