ec30438e245cd170bcd6de352a1f9ee29bfd10bf
Leonid Medovyy <leonid@logik.works>
2019-10-13 15:16:06 -0700

    saving menu color for discussions

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -6,4 +6,31 @@ class Discussion < Entry
 
   validates_presence_of :category
   paginates_per 25
+
+  content_attr :menu_color, :string
+
+  after_touch :update_menu_color
+
+  # setting menu color to black unless
+  # the first header has a color assigned to it
+  def update_menu_color
+    area_found = false
+    self.areas.each do |area|
+      unless area_found
+        if area.type == 'Areas::Header'
+          area_color = area.elements.first.color
+
+          if area_color
+            self.menu_color = area_color
+          else
+            self.menu_color = 'black'
+          end
+
+          self.save
+          area_found = true
+        end
+      end
+    end
+  end
+
 end
