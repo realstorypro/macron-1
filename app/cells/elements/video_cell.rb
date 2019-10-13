116493cb4f429f067039b8b8b278dc3f6327f7e2
Leonid Medovyy <leonid@logik.works>
2019-10-13 14:12:28 -0700

    added the video element

---
+++ app/cells/elements/video_cell.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Elements
+  class VideoCell < HeaderCell
+    def header_size
+      "fullscreen seventy mobile"
+    end
+
+    def title_extra_classes
+      "video"
+    end
+
+    def title_alignment
+      "center"
+    end
+  end
+end
---
+++ app/models/elements/video.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Elements
+  class Video < Element
+    include Autoloadable
+  end
+end
