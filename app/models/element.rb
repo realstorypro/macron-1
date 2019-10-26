0cc8ea3a791742ac046a1120869508733bd4c11e
Leonid Medovyy <leonid@logik.works>
2019-10-25 23:01:11 -0700

    automatically set position for the element

--- app/models/element.rb
+++ app/models/element.rb
@@ -6,6 +6,14 @@ class Element < ApplicationRecord
   belongs_to :elementable, polymorphic: true, optional: true, touch: true
   has_many :elements, -> { order(:position) }, as: :elementable, dependent: :destroy
 
+  after_commit :set_sort
+
+  def set_sort
+    if self.elementable
+      update_column 'position', self.elementable.elements.count
+    end
+  end
+
   def self.policy_class
     MetaPolicy
   end
