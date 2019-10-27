86e2dfc9966eb9f6f2d2adbfc6cd8fc32c1ebf89
Leonid Medovyy <leonid@logik.works>
2019-10-26 20:54:28 -0700

    formating for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 if User.find_by_email("support@goodlogik.com").nil?
   puts "creating a support user with admin privilidges"
-
   password = "id3aman@#{rand(1..5)}"
   user = User.new
   user.email = "support@goodlogik.com"
