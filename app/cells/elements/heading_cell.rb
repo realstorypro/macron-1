18dd18aa6f69a9368ca88f787946cc84420f9ab3
Leonid Medovyy <leonid@logik.works>
2019-11-29 08:17:54 -0800

    updated header sizes

--- app/cells/elements/heading_cell.rb
+++ app/cells/elements/heading_cell.rb
@@ -5,5 +5,10 @@ module Elements
     def amped
       return true if options.respond_to?(:amped)
     end
+
+    def alignment
+      return "#{model.alignment} aligned" unless model.alignment.nil?
+      ''
+    end
   end
 end
